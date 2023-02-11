cfab702e099a6468478f2da08289596d2ece868f
Sophia <116626295+sofiyakamalova@users.noreply.github.com>
2023-02-11 15:49:56 +0600

    I hope i will get 100 pleasee!!!

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,4 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
     }
+        public void find (double a, double b){
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+    }
 }
